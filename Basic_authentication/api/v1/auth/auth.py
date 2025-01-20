a51b9548c9f51602bbaa5377cb101e2c3397fe78
Skomaiya <s.komaiya@alustudent.com>
2025-01-20 13:05:11 +0200

    Add comment to functions

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,17 @@ from typing import List
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that should return True if the path is not in the list of
+            strings excluded_paths
+        """
         return False, path, excluded_paths
     
     def authorization_header(self, request=None) -> str:
+        """ Method that should return None - request or the value of the header
+        """
         return None, request
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that should return None - request or the user instance
+        """
         return None, request
