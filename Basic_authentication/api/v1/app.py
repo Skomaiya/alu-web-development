07e40048bcbbfb26141d061232571dbca21a6ca3
Skomaiya <s.komaiya@alustudent.com>
2025-01-20 17:06:49 +0200

    Update Task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,9 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = AUTH_TYPE
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth:
+
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
