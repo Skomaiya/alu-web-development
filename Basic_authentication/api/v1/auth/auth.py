1eaad5bc754f6f990895cc3a261dd2ac4bc5088d
Skomaiya <s.komaiya@alustudent.com>
2025-01-20 14:49:39 +0200

    Complete Task 3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,14 +12,14 @@ class Auth:
         """ Method that should return True if the path is not in the list of
             strings excluded_paths
         """
-        return [False, path, excluded_paths]
+        return False
 
     def authorization_header(self, request=None) -> str:
         """ Method that should return None - request or the value of the header
         """
-        return None, request
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method that should return None - request or the user instance
         """
-        return None, request
+        return None
