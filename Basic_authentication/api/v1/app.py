307661809e6f57260dcca6c07c03c3a586c6f6eb
Skomaiya <s.komaiya@alustudent.com>
2025-01-21 13:17:42 +0200

    Validate Pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Route module for the API.
 """
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
-
 
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
@@ -25,34 +24,62 @@ if AUTH_TYPE == "basic_auth":
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    Not found handler.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        str: JSON response indicating not found error.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """
+    Unauthorized handler.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        str: JSON response indicating unauthorized error.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def Forbidden(error) -> str:
-    """ Forbidden handler
+def forbidden(error) -> str:
+    """
+    Forbidden handler.
+
+    Args:
+        error: The error object.
+
+    Returns:
+        str: JSON response indicating forbidden error.
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> None:
-    """ Before request handler
+    """
+    Before request handler to manage authentication.
+
+    Returns:
+        None: Executes request handling or raises errors.
     """
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
