7817bd13e0b44051d8878d6862b32340687cc585
Skomaiya <s.komaiya@alustudent.com>
2025-01-21 12:09:06 +0200

    Uppdate Task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -41,14 +41,14 @@ def Forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
-
+@app.before_request
 def before_request() -> None:
     """ Before request handler
     """
     if auth is None:
         return
-    if auth.require_auth(request.path, ['/api/v1/status/', 
-    '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    if auth.require_auth(request.path, ['/api/v1/status/',
+        '/api/v1/unauthorized/', '/api/v1/forbidden/']):
         return
     if auth.authorization_header(request) is None:
         abort(401)
