5ac47b24e68207a585741b71e0b236034989cc1b
Skomaiya <s.komaiya@alustudent.com>
2025-01-21 13:14:36 +0200

    Validate Pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 """
-Module for the Authentication
+Module for the Authentication.
 """
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """Class for managing Basic API authentication.
-    """
-    
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Extracts the base64 part of the Authorization header.
+    """Class for managing Basic API authentication."""
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        Extracts the base64 part of the Authorization header.
+
+        Args:
+            authorization_header (str): The Authorization header to extract.
+
+        Returns:
+            str: The Base64 part of the header, or None if invalid.
         """
         if authorization_header is None:
             return None
