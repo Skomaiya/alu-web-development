64c923f45b5a26bdbc63fcb2446fc373abe111fd
Skomaiya <s.komaiya@alustudent.com>
2025-01-22 13:18:33 +0200

    Retry Task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -95,8 +95,9 @@ def before_request() -> None:
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) and auth.session_cookie(request) is None:
         abort(401)
+        return None
 
     request.current_user = auth.current_user(request)
 
