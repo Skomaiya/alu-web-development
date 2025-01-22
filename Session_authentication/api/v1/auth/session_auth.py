a882b8d4688464066c2cb174e956098d01860ff1
Skomaiya <s.komaiya@alustudent.com>
2025-01-22 12:35:09 +0200

    Attempt Task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Module for the Authentication.
 """
 import base64
+import uuid
 from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -12,4 +13,20 @@ class SessionAuth(Auth):
     """
     Class for managing Session API authentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id.
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        else :
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
